d430bbe09f02c27ad7304e5c08dfb981b22fd0c9
AndrewWaltman <andrew.w.waltman@gmail.com>
2020-01-30 23:39:27 -0500

    Trying to figure out the Classes and Objects.

---
+++ Movies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deliverable_11
+{
+    class Movies
+    {
+        public string Category { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Deliverable_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //A list of movies, each with their own genre.
+            List<string> titles = new List<string>();
+            titles.Add("The Iron Giant");
+            titles.Add("Spiderman: Into the Spiderverse");
+            titles.Add("Boss Baby");
+            titles.Add("The Godfather");
+            titles.Add("Shaw Shank Redemption");
+            titles.Add("Tough Guys Don't Dance");
+            titles.Add("Get Out");
+            titles.Add("Psycho");
+            titles.Add("The Room (By Tommy Wiseau)");
+            titles.Add("Star Wars");
+            titles.Add("Star Trek");
+            titles.Add("Space Balls");
+
+            Console.WriteLine("This is the Movie List Application 1.0");
+            Console.WriteLine("We have a good selection of movies to choose from. Which genre are you interested in?");
+            string genreInput = Console.ReadLine().Trim().ToLower();
+        
+        }
+
+    }
+}
