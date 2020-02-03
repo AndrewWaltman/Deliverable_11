942d0962dafd9a13f2c0bb97aed3c33ff84103af
Andrew Waltman <Andrew.W.Waltman@gmail.com>
2020-02-03 17:30:52 -0500

    Updated 2.3.20

--- Movies.cs
+++ Movies.cs
@@ -6,7 +6,16 @@ namespace Deliverable_11
 {
     class Movies
     {
+
+        //I don't even know if I did any of this right. I don't know if I actually have to set get; and set; as something more or not.
+        //I want to understand this stuff, because I guaruntee it is extremely helpful once you figure it out. Currently though, I dont know what to do.
         public string Category { get; set; }
         public string Title { get; set; }
+        
+        public Movies(string Category, string Title)
+        {
+            this.Category = Category;
+            this.Title = Title;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace Deliverable_11
     {
         static void Main(string[] args)
         {
+            //I do not know how this works.
+            //I don't know if what I'm missing. What parameters need to be set, and how to set them. 
+            //I don't even know how to return the Continue() method wehen the answer is "y" without creating a new method to take its place.
+            //I'm hoping things will clear up in future classes, but currently I am stuck.
+            
 
             //A list of movies, each with their own genre.
             List<string> titles = new List<string>();
@@ -23,11 +28,35 @@ namespace Deliverable_11
             titles.Add("Star Trek");
             titles.Add("Space Balls");
 
-            Console.WriteLine("This is the Movie List Application 1.0");
-            Console.WriteLine("We have a good selection of movies to choose from. Which genre are you interested in?");
-            string genreInput = Console.ReadLine().Trim().ToLower();
-        
-        }
+            Console.WriteLine("Welcome to the Movie Lit st Application");
+            Console.WriteLine("Which genre of movie are you looking for?");
+            string categoryInput = Console.ReadLine().ToLower().Trim();
 
+            Movies a = new Movies(categoryInput, "");
+
+            Continue();
+        }
+        
+        //Method for continuing or not.
+        public static bool Continue()
+        {
+            Console.WriteLine("Would you like to continue? (Y/N)");
+            string inputYN = Console.ReadLine().ToLower().Trim();
+
+            if (inputYN == "y")
+            {
+                return true;
+            }
+            else if (inputYN == "n")
+            {
+                Console.WriteLine("Thank you.");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid response, please just type 'y' or 'n'");
+                return Continue();
+            }
+        }
     }
 }
