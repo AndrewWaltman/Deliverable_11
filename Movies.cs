65f71a72223976f8dd648958fa873236555973f1
AndrewWaltman <andrew.w.waltman@gmail.com>
2020-02-04 01:38:43 -0500

    Late Lab 11 Practice (2.04.20)

--- Movies.cs
+++ Movies.cs
@@ -4,18 +4,23 @@ using System.Text;
 
 namespace Deliverable_11
 {
-    class Movies
+    class Movie
     {
-
-        //I don't even know if I did any of this right. I don't know if I actually have to set get; and set; as something more or not.
-        //I want to understand this stuff, because I guaruntee it is extremely helpful once you figure it out. Currently though, I dont know what to do.
+        //Here is where the magic happens.
+        //We set Category and Title as there own Objects that have a get and set parameter.
+        //We do not need these parameters to do anything for this lab, so we are fine leaving them as get; and set;
         public string Category { get; set; }
         public string Title { get; set; }
         
-        public Movies(string Category, string Title)
+        //THIS however is Movie, and uses both of the above Objects to create the rest.
+        //This Object needs two parameters for the Lab, and we set those as string title, and category.
+        //Because of this, whenever we need a new Movie, we need to have both parameters met.
+        //Secondly, we convert the Object Parameters Category and Title to equal their respective string name.
+        //We do this so that when we label these strings with new Movie's, we can use the objects above to get and set specific things.
+        public Movie(string title, string category)
         {
-            this.Category = Category;
-            this.Title = Title;
+            this.Category = category;
+            this.Title = title;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,37 +7,67 @@ namespace Deliverable_11
     {
         static void Main(string[] args)
         {
-            //I do not know how this works.
-            //I don't know if what I'm missing. What parameters need to be set, and how to set them. 
-            //I don't even know how to return the Continue() method wehen the answer is "y" without creating a new method to take its place.
-            //I'm hoping things will clear up in future classes, but currently I am stuck.
-            
-
-            //A list of movies, each with their own genre.
-            List<string> titles = new List<string>();
-            titles.Add("The Iron Giant");
-            titles.Add("Spiderman: Into the Spiderverse");
-            titles.Add("Boss Baby");
-            titles.Add("The Godfather");
-            titles.Add("Shaw Shank Redemption");
-            titles.Add("Tough Guys Don't Dance");
-            titles.Add("Get Out");
-            titles.Add("Psycho");
-            titles.Add("The Room (By Tommy Wiseau)");
-            titles.Add("Star Wars");
-            titles.Add("Star Trek");
-            titles.Add("Space Balls");
-
-            Console.WriteLine("Welcome to the Movie Lit st Application");
-            Console.WriteLine("Which genre of movie are you looking for?");
-            string categoryInput = Console.ReadLine().ToLower().Trim();
-
-            Movies a = new Movies(categoryInput, "");
-
-            Continue();
+            //Welcomes user.
+            Console.WriteLine("Welcome to the Movie List Application");
+            //Immediately runs Menu();
+            Menu();
         }
-        
-        //Method for continuing or not.
+        public static bool Menu()
+        {
+            //When we see this, we have to understand what is happening in Movies.cs
+            // ==================See Movies.cs for more============================
+            //Once we get that, we realize that Movies has to have Two Strings: Title and Category.
+            //This works similarly to parameters in methods!
+            //And with this, we have to have a title and category for each movie.
+            Movie ironGiant = new Movie("The Iron Giant", "animated");
+            Movie spiderman = new Movie("Spiderman: Into the Spiderverse", "animated");
+            Movie bossBaby = new Movie("Boss Baby", "animated");
+            Movie theGodfather = new Movie("The Godfather", "drama");
+            Movie shawShank = new Movie("Shaw Shank Redemption", "drama");
+            Movie toughGuys = new Movie("Tough Guys Don't Dance", "drama");
+            Movie getOut = new Movie("Get Out", "horror");
+            Movie psycho = new Movie("Psycho", "horror");
+            Movie theRoom = new Movie("The Room (By Tommy Wiseau)", "horror");
+            Movie starWars = new Movie("Star Wars", "scifi");
+            Movie starTrek = new Movie("Star Trek", "scifi");
+            Movie spaceBalls = new Movie("Space Balls", "scifi");
+
+            //We create this list of titles with the Movie names above.
+            List<Movie> title = new List<Movie> { ironGiant, spiderman, bossBaby, theGodfather, shawShank, toughGuys, getOut, psycho, theRoom, starWars, starTrek, spaceBalls };
+
+            //Create a bool to return to later.
+            bool menu = true;
+            //Create a while loop to continue while true when going through the later if and for statements.
+            while (menu)
+            {
+                Console.WriteLine("Which genre of movie are you looking for?");
+                Console.WriteLine("Animated");
+                Console.WriteLine("Drama");
+                Console.WriteLine("Horror");
+                Console.WriteLine("SciFi");
+                string input = Console.ReadLine().ToLower().Trim();
+
+                Console.WriteLine("Here are the movies we have under that category.\n==============================");
+
+                //This is where we reapply the Objects from earlier.
+                //First we have the regular "i formula" for for loops.
+                //Just this time we are using the "title" count from Movies.
+                for (int i = 0; i < title.Count; i++)
+                {
+                    Movie m = title[i]; //Sets a Movie type (m) to use for each title[i](searches the current title name) fot the next statement.
+                    if (m.Category == input) //if the user input matches a Movie Category, we are set to write out the Title.
+                    {            // Also works with title[i] and instead of naming EVERY title, it just names the input matched title.
+                        Console.WriteLine(m.Title); //Writes out the Movie Title.
+                    }
+                }
+                Console.WriteLine("==============================");
+                break;
+                //Ends the conditional.
+            }
+            //Continues to whether or not the user wishes to continue.
+            return Continue();
+        }
+        //Continue method I've already seen before.
         public static bool Continue()
         {
             Console.WriteLine("Would you like to continue? (Y/N)");
@@ -45,7 +75,8 @@ namespace Deliverable_11
 
             if (inputYN == "y")
             {
-                return true;
+                Console.Clear();
+                return Menu();
             }
             else if (inputYN == "n")
             {
@@ -60,3 +91,7 @@ namespace Deliverable_11
         }
     }
 }
+
+
+            
+       
